737f5d192a8b9b8d793f7b4e38c000bd69851b87
CyberPlatoon <cyberplatoon24.7@gmail.com>
2024-08-28 22:39:39 +0530

    added index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project-X</title>
+    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<script src="bootstrap/js/bootstrap.min.js"></script>
+<script src="bootstrap/js/jquary.js"></script>
+
+<main>
+    <div class="container-fluid bg-danger">
+        <div class="container bg-dark NavBar ">
+            <div class="card">
+                <h2>Vdd</h2>
+            </div>
+        </div>
+    </div>
+</main>
+    
+</body>
+</html>
